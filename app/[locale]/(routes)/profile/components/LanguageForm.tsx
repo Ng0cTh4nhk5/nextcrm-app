34a04ddd3f625b4fb52a03a0bf990eb4cc474f98
Ng0cTh4nhk5 <ng0cthanh@gmail.com>
2026-07-16 20:48:12 +0700

    feat(i18n): enable Vietnamese language selection and localization

--- app/[locale]/(routes)/profile/components/LanguageForm.tsx
+++ app/[locale]/(routes)/profile/components/LanguageForm.tsx
@@ -25,6 +25,7 @@ export function LanguageForm({ userId }: { userId: string }) {
     { label: t("czech"), value: "cz" },
     { label: t("german"), value: "de" },
     { label: t("ukrainian"), value: "uk" },
+    { label: t("vietnamese"), value: "vi" },
   ];
 
   async function handleChange(language: string) {
